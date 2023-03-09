9b30f4c9a0067461afb601810beeb029148068aa
duoykb <duoykb@gmail.com>
2023-03-09 11:28:31 +0300

    ...

--- app/Program.cs
+++ app/Program.cs
@@ -4,6 +4,11 @@ using Spectre.Console;
 var dtable = DtableBuilder.FromCsv(@"C:\Users\kb\Desktop\WD\myprojects\duoydata\app\csvFiles\sample-1.csv");
 var cmd = new DtableCommands{Dtable = dtable};
 
-string? input;
 AnsiConsole.Write(new FigletText("duoy Data"));
-while ((input = AnsiConsole.Ask<string>(">>> ")).Trim().ToLower() is not null or "exit") cmd.Execute(input);
+while (true)
+{
+    var input = AnsiConsole.Ask<string>(">>> ").Trim().ToLower();
+    if (input is "exit")
+        return;
+    cmd.Execute(input);
+}
