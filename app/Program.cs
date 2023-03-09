d4f671394b8d243da831488d19c9ef3fa9999345
duoykb <duoykb@gmail.com>
2023-03-09 10:24:35 +0300

    ...

--- app/Program.cs
+++ app/Program.cs
@@ -5,5 +5,6 @@ var dtable = DtableBuilder.FromCsv(@"C:\Users\kb\Desktop\WD\myprojects\duoydata\
 var cmd = new DtableCommands{Dtable = dtable};
 
 string? input;
-
-while ((input = AnsiConsole.Ask<string>(">>> "))?.Trim().ToLower() is not null or "exit") cmd.Execute(input);
+var txt = new FigletText("duoy Data");
+AnsiConsole.Write(txt);
+while ((input = AnsiConsole.Ask<string>(">>> ")).Trim().ToLower() is not null or "exit") cmd.Execute(input);
